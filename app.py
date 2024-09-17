91aab773c6cbe54b383a898ff8c3b636a51a216d
Maxim <maxim.oweyssi@est.org.uk>
2024-09-17 17:34:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Dynamic DataFrame with Editable Column")
 
 # Slider to select the number of rows
 num_rows = st.number_input('Set the number of rows', min_value=1, max_value=100, value=5, step=1)
-
+api_key = st.text_input('Mapy.cz API klic:')
 # Create an empty DataFrame with three columns and the number of rows selected
 df = pd.DataFrame({
     'Adresa': [None] * num_rows,
