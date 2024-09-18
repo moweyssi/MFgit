c0c28e886122f198da8062c5c60158006bab47fc
Maxim <maxim.oweyssi@est.org.uk>
2024-09-18 06:44:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_address(address_string):
     if response['items']==[]:
         return None
     else:
-        text_address = response['items'][0]['name']
+        
         regional_address = response['items'][0]['regionalStructure'][0]['name'].split('/')
         # Check if the split resulted in exactly two parts
         if len(regional_address) == 2:
@@ -38,12 +38,12 @@ def get_address(address_string):
         else:
             cislo_domovni = regional_address[0]
             cislo_orientacni = -1
-        
         nazev_ulice =   response['items'][0]['regionalStructure'][1]['name']
         nazev_casti_obce = response['items'][0]['regionalStructure'][2]['name']
         nazev_obce = response['items'][0]['regionalStructure'][3]['name']
         psc = response['items'][0]['zip'].replace(' ','')
 
+        text_address = ', '.join([nazev_obce,nazev_casti_obce,nazev_ulice,regional_address,psc])
         query_vector = np.array([nazev_obce, nazev_casti_obce, nazev_ulice,cislo_domovni,cislo_orientacni,psc])
         return query_vector, text_address
 
