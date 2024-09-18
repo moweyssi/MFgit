a59f249444e431cfed67c28c54837d2f8d14d3b2
Maxim <maxim.oweyssi@est.org.uk>
2024-09-18 06:53:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,13 +76,11 @@ def get_match(address):
         return closest_match.argmax(), text_address
 
 # Title of the app
-st.title("Dynamic DataFrame with Editable Column")
+st.title("Kod Adresniho Mista")
 
-# Slider to select the number of rows
-num_rows = st.number_input('Set the number of rows', min_value=1, max_value=100, value=1, step=1)
 # Create an empty DataFrame with three columns and the number of rows selected
 df = pd.DataFrame({
-    'Adresa': [None] * num_rows
+    'Adresa': [None]
 })
 editable_df = st.data_editor(df, num_rows="dynamic", key="editable_df",use_container_width =True)
 
